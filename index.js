01de9f6bcb022ed0d63d437f2448ee5ed8800c47
Jakub Polak <ypolak.00@gmail.com>
2021-11-08 18:12:00 +0100

    Almost there, moving from spawn to drag area doesn't align box properly

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ import interact from 'https://cdn.interactjs.io/v1.10.11/interactjs/index.js'
 // target elements with the "draggable" class
 interact('.draggable')
   .draggable({
-    // enable inertial throwing
+    // disable inertial throwing
     inertia: false,
     // keep the element within the area of it's parent
     modifiers: [
       interact.modifiers.restrictRect({
-        restriction: 'parent',
+        restriction: 'body',
         endOnly: true
       })
     ],
-    // enable autoScroll
-    autoScroll: true,
+    // disable autoScroll
+    autoScroll: false,
 
     listeners: {
       // call this function on every dragmove event
       move: dragMoveListener,
 
       // call this function on every dragend event
-      end (event) {
+      /*end (event) {
         var textEl = event.target.querySelector('p')
 
         textEl && (textEl.textContent =
@@ -28,9 +28,42 @@ interact('.draggable')
           (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
                      Math.pow(event.pageY - event.y0, 2) | 0))
             .toFixed(2) + 'px')
-      }
+      }*/
     }
   })
+
+interact('.drop_target')
+.dropzone({
+  ondrop: function (event) {
+    /*
+    alert(event.relatedTarget.id
+          + ' was dropped into '
+          + event.target.id)
+    */
+    
+    var rect = event.relatedTarget.getBoundingClientRect();
+    if (event.target.id == "drag_zone" && event.target.id != event.relatedTarget.parentNode.id)
+    {
+      // TO DO: find how to get height of the element, and subtract from y
+      console.log(event.relatedTarget.style.height)
+      event.relatedTarget.style.transform = 'translate('+ rect.left +'px, '+ rect.top +'px)'
+      event.relatedTarget.setAttribute('data-x', rect.left)
+      event.relatedTarget.setAttribute('data-y', rect.top - event.relatedTarget.style.height)
+      event.relatedTarget.style.transform = 'translate('+ rect.left +'px, '+ rect.top +'px)'
+      $('#' + event.relatedTarget.id).appendTo($('#' + event.target.id))
+    }
+    if (event.target.id != "drag_zone" && event.target.id != event.relatedTarget.parentNode.id)
+    {
+      event.relatedTarget.setAttribute('data-x', 0)
+      event.relatedTarget.setAttribute('data-y', 0)
+      event.relatedTarget.style.transform = 'translate(0px, 0px)'
+      $('#' + event.relatedTarget.id).prependTo($('#' + event.target.id))
+    }
+  }
+})
+.on('dropactivate', function (event) {
+  event.target.classList.add('drop-activated')
+})
 
 function dragMoveListener (event) {
   var target = event.target
@@ -45,6 +78,3 @@ function dragMoveListener (event) {
   target.setAttribute('data-x', x)
   target.setAttribute('data-y', y)
 }
-
-// this function is used later in the resizing and gesture demos
-window.dragMoveListener = dragMoveListener
