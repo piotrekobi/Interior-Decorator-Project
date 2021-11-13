a2a2652f7c370ef68c3acfd3cea02438ea7e2158
Kamil Łukasz Szydłowski <k.l.szydlowski@stud.elka.pw.edu.pl>
2021-11-13 20:21:08 +0100

    Fix no static files in Azure

--- wnetrza/settings.py
+++ wnetrza/settings.py
@@ -118,4 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "/static/")
+STATIC_ROOT = os.environ.get("DJANGO_STATIC_ROOT", "./static/")
