e7e2a6bd695eb4f96a053a2d7edc6f051232b063
Kamil Łukasz Szydłowski <k.l.szydlowski@stud.elka.pw.edu.pl>
2021-11-13 20:27:31 +0100

    Fix no static files in Azure

--- wnetrza/settings.py
+++ wnetrza/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    "whitenoise.runserver_nostatic",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,3 +122,6 @@ USE_TZ = True
 
 STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "/static/")
 STATIC_ROOT = os.environ.get("DJANGO_STATIC_ROOT", "./static/")
+FRONTEND_DIR = "app"
+STATICFILES_DIRS = [os.path.join(FRONTEND_DIR, 'build', 'static')]
+STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
