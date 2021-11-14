4ebb51bf4e3e6574cade86d4d8be3b4f9088c28c
Kamil Łukasz Szydłowski <k.l.szydlowski@stud.elka.pw.edu.pl>
2021-11-14 10:57:32 +0100

    Add example read/write functionality

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'app'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^example_rw/', views.example_rw, name='example_rw'),
+    url(r'^example_communication/', views.example_communication, name='example_communication'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
+from django.http import HttpResponse
 
 
 def index(request):
     return render(request, 'app/index.html')
+
+
+def example_rw(request):
+    return render(request, 'app/example_rw.html')
+
+
+def example_communication(request):
+    return HttpResponse("Example communication")
