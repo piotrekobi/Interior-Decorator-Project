0dc07e7790849b69ba1eda9fcd1ec005a53424f8
Miosz Pawe Kasak <m.kasak@stud.elka.pw.edu.pl>
2021-12-03 22:09:35 +0100

    Fixed dragability for dynamically created rectangles Removed default rectangles

--- app/static/app/js/rectangle.js
+++ app/static/app/js/rectangle.js
@@ -1,8 +1,12 @@
-function addRectangle(width, height, color) {
+function addRectangle(width, height, color, spawn_zone_id) {
     var div = document.createElement("div");
+    var spawn_zone = document.getElementById(spawn_zone_id);
     div.className = "draggable rectangle-style ";
+    var number_of_rectangles = document.querySelectorAll('[id^="rectangle"]').length;
+    
+    div.id = "rectangle"+number_of_rectangles;
     div.style.width = width;    
     div.style.height = height;
     div.style.background = color;
-    document.getElementById("spawn_zone").appendChild(div);
+    spawn_zone.appendChild(div);
 };
