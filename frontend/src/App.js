e6026774a2c70b71c2d69c6353d3a633d8b7ccb1
Piotr Jan Paturej <p.paturej@stud.elka.pw.edu.pl>
2021-12-20 18:25:33 +0100

    Add working rectangle menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,53 @@
-import './App.css';
-import { Component, createRef } from 'react';
-import MenuZone from './components/MenuZone';
-import DragZone from './components/DragZone';
-import SpawnZone from './components/SpawnZone';
-import WallPicker from './components/WallPicker';
+import "./App.css";
+import { Component, createRef } from "react";
+import MenuZone from "./components/MenuZone";
+import DragZone from "./components/DragZone";
+import SpawnZone from "./components/SpawnZone";
+import WallPicker from "./components/WallPicker";
+import RectangleMenu from "./components/RectangleMenu";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.dragZone = createRef();
     this.spawnZone = createRef();
     this.wallPicker = createRef();
+    this.rectangleMenu = createRef();
   }
 
   handleWallSelection = (specs) => {
     this.dragZone.current.drawWall(specs);
-  }
+  };
 
   handleWallsClick = () => {
     this.wallPicker.current.toggleModal();
-  }
+  };
 
   handleRectanglesClick = () => {
-    this.spawnZone.current.addRectangle('100px', '100px', 'gray');
-  }
+    this.rectangleMenu.current.toggleModal();
+  };
+
+  handleAddRectangleClick = (width, height, color) => {
+    this.spawnZone.current.addRectangle(width, height, color);
+  };
 
   render() {
     return (
       <body>
-        <MenuZone onWallsClick={this.handleWallsClick}
-                  onRectanglesClick={this.handleRectanglesClick}/>
-        <DragZone ref={this.dragZone}/>
-        <SpawnZone ref={this.spawnZone}/>
+        <MenuZone
+          onWallsClick={this.handleWallsClick}
+          onRectanglesClick={this.handleRectanglesClick}
+        />
+        <DragZone ref={this.dragZone} />
+        <SpawnZone ref={this.spawnZone} />
 
-        <WallPicker ref={this.wallPicker} onWallSelection={this.handleWallSelection}/>
+        <WallPicker
+          ref={this.wallPicker}
+          onWallSelection={this.handleWallSelection}
+        />
+            <RectangleMenu ref={this.rectangleMenu}
+          onAddRectangleClick={this.handleAddRectangleClick}
+          />
       </body>
     );
   }
