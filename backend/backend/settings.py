3148e2c030ff049ea3aadbcd90a48871d6e0878d
Kamil Szydłowski <kamil.szydlowski.stud@pw.edu.pl>
2022-01-01 11:48:36 +0100

    Prepare backend for optimization algorithm

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,13 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
-    'rest_framework',
     'interiors',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,7 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-CORS_ORIGIN_WHITELIST = [
-     'http://127.0.0.1:3000',
-]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,13 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
 from interiors import views
-
-router = routers.DefaultRouter()
-router.register(r'interiors', views.InteriorsView, 'interiors')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
 ]
